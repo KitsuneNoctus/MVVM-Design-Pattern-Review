f108d13978d4cfd8c045f5df760272e1d5cd3cba
KitsuneNoctus <theceintiant@gmail.com>
2024-07-11 15:18:12 -0700

    Creating cells for table view

---
+++ GoodNews/GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Henry Calderon on 7/11/24.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
