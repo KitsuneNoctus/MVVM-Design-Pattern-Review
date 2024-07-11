31ca036466e5df5e130a568c33816a33cf6cc539
KitsuneNoctus <theceintiant@gmail.com>
2024-07-11 16:30:55 -0700

    Added basic UI

---
+++ HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffee
+//
+//  Created by Henry Calderon on 7/11/24.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffee
+//
+//  Created by Henry Calderon on 7/11/24.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
