16b03b24aa36838b50176d4fbe142fd29144f71d
KitsuneNoctus <theceintiant@gmail.com>
2024-07-11 16:22:32 -0700

    Starting Hot Coffee project

---
+++ HotCoffee/HotCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotCoffee
+//
+//  Created by Henry Calderon on 7/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
