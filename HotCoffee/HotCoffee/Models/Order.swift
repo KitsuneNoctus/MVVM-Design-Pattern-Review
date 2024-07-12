a7df2182d7a57146cde8c58470260200cb0e08b4
KitsuneNoctus <theceintiant@gmail.com>
2024-07-12 10:48:49 -0700

    Creating coffee orders

---
+++ HotCoffee/HotCoffee/Models/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by Henry Calderon on 7/12/24.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case esspressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeType
+}
