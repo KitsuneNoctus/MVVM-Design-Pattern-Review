d5d78f42489089c635e1065258ab889feae0c284
KitsuneNoctus <theceintiant@gmail.com>
2024-07-13 15:03:32 -0700

    Adding stylings to the app

---
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Henry Calderon on 7/13/24.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Henry Calderon on 7/13/24.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+}
