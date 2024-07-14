8c834569d13315ad48be92b1eed2af79d6fd3939
KitsuneNoctus <theceintiant@gmail.com>
2024-07-14 14:58:39 -0700

    Setup the cells and demo with hard coded values

---
+++ GoodWeather/GoodWeather/CustomCells/WeatherCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by Henry Calderon on 7/14/24.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
