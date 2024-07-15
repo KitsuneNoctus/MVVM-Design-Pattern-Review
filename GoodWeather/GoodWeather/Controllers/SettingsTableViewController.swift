f7981ff4ca03317650a2f091f45cd6e546cebbc2
KitsuneNoctus <theceintiant@gmail.com>
2024-07-15 12:28:22 -0700

    Setting up settings views. Not gonna dowload his STARTerProJEct

---
+++ GoodWeather/GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather
+//
+//  Created by Henry Calderon on 7/15/24.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
