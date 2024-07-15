25a70bdf56afd53c550e1eef7f1bd9473e313a84
KitsuneNoctus <theceintiant@gmail.com>
2024-07-15 13:15:10 -0700

    Setting up segues and delegates for the settings

--- GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -52,6 +52,21 @@ class WeatherListTableViewController: UITableViewController, AddWeatherDelegate 
         if segue.identifier == "AddWeatherCityViewController" {
             prepareSegueForAddWeatherCityViewController(segue: segue)
         }
+        else if segue.identifier == "SettingsTableViewController" {
+            prepareSegueForSettingsTableViewController(segue: segue)
+        }
+    }
+    
+    func prepareSegueForSettingsTableViewController(segue: UIStoryboardSegue) {
+        guard let nav = segue.destination as? UINavigationController else {
+            fatalError("NavigationController not found")
+        }
+        
+        guard let settingsTVC = nav.viewControllers.first as? SettingsTableViewController else {
+            fatalError("SettingsTableViewController not found")
+        }
+        
+        settingsTVC.delegate = self
     }
     
     func prepareSegueForAddWeatherCityViewController(segue: UIStoryboardSegue) {
@@ -64,5 +79,11 @@ class WeatherListTableViewController: UITableViewController, AddWeatherDelegate 
         }
         
         addWeatherCityVC.delegate = self
+    }
+}
+
+extension WeatherListTableViewController: SettingsDelegate {
+    func settingsDone(vm: SettingsViewModel) {
+        
     }
 }
