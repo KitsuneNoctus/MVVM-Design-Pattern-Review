f365a0eded7ad98421006124af2bb144a10a07de
KitsuneNoctus <theceintiant@gmail.com>
2024-07-15 17:24:23 -0700

    Adding in tests

---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by Henry Calderon on 7/15/24.
+//
+
+import XCTest
+
+final class CalculatorAppTests: XCTestCase {
+
+    
+
+}
