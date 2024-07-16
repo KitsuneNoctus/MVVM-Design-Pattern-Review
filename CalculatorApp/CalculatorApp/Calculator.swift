8e92b54b8216d64fc2258d6212c7441557dc705a
KitsuneNoctus <theceintiant@gmail.com>
2024-07-15 17:38:57 -0700

    some basic test setup and test

---
+++ CalculatorApp/CalculatorApp/Calculator.swift
@@ -0,0 +1,19 @@
+//
+//  Calculator.swift
+//  CalculatorApp
+//
+//  Created by Henry Calderon on 7/15/24.
+//
+
+import Foundation
+
+
+class Calculator {
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+}
--- CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -6,9 +6,21 @@
 //
 
 import XCTest
+@testable import CalculatorApp
 
 final class CalculatorAppTests: XCTestCase {
-
     
+    func test_SubtractTwoNumbers() {
+        let calculator = Calculator()
+        let result = calculator.subtract(5, 2)
+        XCTAssertEqual(result, 3)
+    }
+
+    func test_AddTwoNumbers() {
+        let calculator = Calculator()
+        let result = calculator.add(2,3)
+        
+        XCTAssertEqual(result, 5)
+    }
 
 }
